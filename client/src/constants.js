f2ef4a21dd211eeeefb034780f71b73dc6f5a063
Giovanni <giovanni_vidotto@hotmail.it>
2020-03-13 22:11:46 +0100

    heroku tokyo esagono

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,7 +2,7 @@ const isDev = process.env.NODE_ENV !== 'production';
 
 const HOST = {
   development: 'http://localhost:8080',
-  production: 'https://app-tokyo.herokuapp.com',
+  production: 'https://tokyoesagono.herokuapp.com',
 };
 
 export const SITE_URL = isDev ? HOST.development : HOST.production;
